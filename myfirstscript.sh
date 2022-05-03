8ff675a77de4b1b5fb3820a78fb0336a2c06cac9
AntonSergienko <103580242+AntonSergienko@users.noreply.github.com>
2022-05-03 12:12:12 +0300

    Add files via upload

---
+++ myfirstscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "VADIM KENZO"
+cd D:/qatest/myfirstscript;
+mkdir qa_111 qa_222 qa_333;
+cd D:/qatest/myfirstscript;
+touch 1.txt 2.txt 3.txt 1.json 2.json;
+mkdir qa_111 qa_222 qa_333;
+ls -f;
+
+
